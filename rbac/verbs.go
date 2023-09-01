c8a5cf92ad50ff4d6f4c885bd99cc86923170c05
kj22k0m <andrew345345@yandex.ru>
2023-09-01 21:53:11 +0300

    feat(rbac): added rbac verbs

---
+++ rbac/verbs.go
@@ -0,0 +1,12 @@
+package rbac
+
+// Based on Kubernetes verbs.
+const (
+	Get    = "get"
+	Create = "create"
+	Delete = "delete"
+	Update = "update"
+	List   = "list"
+	Patch  = "patch"
+	Watch  = "watch"
+)
