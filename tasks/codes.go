d5fdcfcd37f2a0aae3dea472431f78e7bcb9e518
kj22k0m <andrew345345@yandex.ru>
2023-09-10 18:45:30 +0300

    chore(tasks): added states for tasks

---
+++ tasks/codes.go
@@ -0,0 +1,11 @@
+package tasks
+
+const (
+	StatePending   = "pending"
+	StateScheduled = "scheduled"
+	StateRunning   = "running"
+	StateCancelled = "cancelled"
+	StateStopped   = "stopped"
+	StateCompleted = "completed"
+	StateFailed    = "failed"
+)
